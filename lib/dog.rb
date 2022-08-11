1ea0595c916a56c500d35873923ccd32c92d6e93
Hannah Glazier <hrayglazier@aol.com>
2022-08-11 11:48:01 -0600

    commit tree 1f01dfe20d0ab53fc709ed13813702cd0fc86f19

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
 
+    # SETTER NAME
+    def name=(name)
+        @name = name
+    end
+
+    # GETTER NAME
+    def name
+        @name
+    end
+
+    # SETTER BREED
+    def breed=(breed)
+        @breed = breed
+    end
+
+    # GETTER BREED
+    def breed
+        @breed
+    end
+
 end
